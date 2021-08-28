83e0fc7ba9f50026c0ac20864e85d2a050f40913
nathansilva <rm88517@fiap.com.br>
2021-08-28 20:19:35 -0300

    Menu.jsx criado

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -5,7 +5,22 @@ import './Menu.css'
 
 export default function menu() {
   return(
-      <nav className='menu'>
+    <header className='main-header'>
+      <nav>
+        <ul className='ul-menu'>
+          <li className='elemento'><a className='main-action red-color' href="#">Nacional</a></li>
+          <li><a className='main-action' href="#">Business</a></li>
+          <li><a className='main-action' href="#">Internacional</a></li>
+          <li><a className='main-action bold-color' href="#">Saúde</a></li>
+          <li><a className='main-action' href="#">Tecnologia</a></li>
+          <li><a className='main-action' href="#">Esporte</a></li>
+          <li><a className='main-action' href="#">Entretenimento</a></li>
+          <li><a className='main-action' href="#">Estilo</a></li>
+          <li><a className='main-action' href="#">Viagem &amp; Gastronomia</a></li>
+          <li><a className='main-action' href="#">Newsletters</a></li>
+          <li><a className='main-action' href="#">Podcasts</a></li>
+        </ul>
       </nav>
+    </header>
   );
 }
