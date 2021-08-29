988d6b8db987f3cf09e594349888e3cc188bafd4
nathansilva <rm88517@fiap.com.br>
2021-08-29 19:24:49 -0300

    Internacional

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -12,11 +12,11 @@ export default function Menu() {
         <ul className={active ? 'ul-menu-mobile': 'ul-menu'} onClick={() => setModal(false)}>
           <li className='element'><a className='main-action red-element' href="#">Nacional</a></li>
           <li><a className='main-action' href="#">Business</a></li>
-          <li><a className='main-action' href="#">Internacional</a></li>
+          <li><a className='main-action' href="https://youtu.be/EbDMNjT-QpI?list=OLAK5uy_nEjgtNBDU83C-Ev-_aDsMHnjZf2YIust0" target='_blank' rel="noreferrer">Internacional</a></li>
           <li><a className='main-action bold-color' href="#">Saúde</a></li>
           <li><a className='main-action' href="#">Tecnologia</a></li>
           <li><a className='main-action' href="#">Esporte</a></li>
-          <li><a className='main-action' href="#">Entretenimento</a></li>
+          <li><a className='main-action' href="https://www.youtube.com/watch?v=XRaX_3qZhA0" target='_blank' rel="noreferrer">Entretenimento</a></li>
           <li><a className='main-action' href="#">Estilo</a></li>
           <li><a className='main-action' href="#">Viagem &amp; Gastronomia</a></li>
           <li><a className='main-action' href="#">Newsletters</a></li>
