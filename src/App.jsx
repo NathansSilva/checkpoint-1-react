18039ad3544e81e4e0ee0f2a23828dfae29f5183
nathansilva <rm88517@fiap.com.br>
2021-08-29 15:44:26 -0300

    App.jsx components linkados

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import react from 'react'
+import React from 'react'
 import './App.css'
 import Header from './components/header/Header'
 import Menu from './components/menu/Menu'
@@ -7,11 +7,11 @@ import Corpo from './components/corpo/Corpo'
 export default function App(){
     return(
         <>
-        <section className='container'>
+        <div className='container'>
             <Menu/>
             <Header/>
             <Corpo/>
-        </section>
+        </div>
         </>
     );
 }
